5fe4fad4bb8843c5fb41572f59a278076d8750c4
汪晓雨 <15124532157@163.com>
2021-06-28 11:06:29 +0800

    init

---
+++ src/Exceptions/Exception.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MrwangTc\QichachaApi\Exceptions;
+
+/**
+ * @Date    : 2021/6/28 11:03
+ * @Author  : Mr.wang
+ * Class Exception
+ * @package MrwangTc\QichachaApi\Exceptions
+ */
+class Exception extends \Exception
+{
+
+}
