359c3eff5dd821a320d7b1b3c91c5798d6b7a253
汪晓雨 <15124532157@163.com>
2021-06-29 11:40:45 +0800

    补充接口

--- src/BaseApi.php
+++ src/BaseApi.php
@@ -8,6 +8,13 @@ use MrwangTc\QichachaApi\Exceptions\InvalidArgumentException;
 use MrwangTc\QichachaApi\Support\Config;
 use MrwangTc\QichachaApi\Support\Helper;
 
+/**
+ * Notes    : 接口基础
+ * @Date    : 2021/6/29 11:21
+ * @Author  : Mr.wang
+ * Class BaseApi
+ * @package MrwangTc\QichachaApi
+ */
 class BaseApi
 {
 
@@ -24,13 +31,13 @@ class BaseApi
     public function __construct(array $config)
     {
         if (empty($config['key'])) {
-            throw new InvalidArgumentException('应用APPKEY不能为空');
+            throw new InvalidArgumentException('应用APPKEY不能为空', 104);
         }
         if (empty($config['SecretKey'])) {
-            throw new InvalidArgumentException('应用SecretKey不能为空');
+            throw new InvalidArgumentException('应用SecretKey不能为空', 104);
         }
         if (empty($config['domain'])) {
-            throw new InvalidArgumentException('接口地址没配置');
+            throw new InvalidArgumentException('接口地址没配置', 104);
         }
         $this->config = new Config($config);
     }
@@ -73,6 +80,27 @@ class BaseApi
         } catch (\Exception $e) {
             $check = preg_match_all('/[\x{4e00}-\x{9fff}]+/u', $e->getmessage(), $cn_name);
             throw new HttpException($cn_name, $e->getCode(), $e);
+        }
+    }
+
+    /**
+     * Notes   : 整合各种不同的数据
+     * @Date   : 2021/6/29 11:34
+     * @Author : Mr.wang
+     * @return mixed
+     * @throws \MrwangTc\QichachaApi\Exceptions\InvalidArgumentException
+     */
+    protected function getData()
+    {
+        if (is_array($this->baseData)) {
+            #todo 未知问题
+        } else {
+            $array = json_decode($this->baseData);
+            if ($array->Status != 200) {
+                throw new InvalidArgumentException($array->Message, $array->Status);
+            } else {
+                return $array->Result;
+            }
         }
     }
 
--- src/MessageQuery.php
+++ src/MessageQuery.php
@@ -3,7 +3,7 @@
 namespace MrwangTc\QichachaApi;
 
 /**
- * Notes    : 工商信息查询
+ * Notes    : 工商信息查询（暂时拿掉所有选填选项，功能预留）
  * @Date    : 2021/6/29 9:25
  * @Author  : Mr.wang
  * Class MessageQuery
@@ -11,20 +11,69 @@ namespace MrwangTc\QichachaApi;
  */
 class MessageQuery extends BaseApi
 {
+
     protected $PageNum;
 
     protected $PageSize;
 
+    /**
+     * Notes   : 企业工商模糊搜索
+     * @Date   : 2021/6/29 11:26
+     * @Author : Mr.wang
+     * @param $title
+     * @return mixed
+     * @throws \MrwangTc\QichachaApi\Exceptions\HttpException
+     * @throws \MrwangTc\QichachaApi\Exceptions\InvalidArgumentException
+     */
     public function fuzzySearch($title)
     {
         $this->getHeader();
         $params = ['searchKey' => $title];
         $this->getParams($params);
         $url            = $this->config['domain'] . '/FuzzySearch/GetList';
-        $this->baseData = $this->dopost($url);
+        $this->baseData = $this->methodGetDopost($url);
+
         return $this->getData();
     }
 
+    /**
+     * Notes   : 企业工商数据查询(多维度查询)
+     * @Date   : 2021/6/29 11:26
+     * @Author : Mr.wang
+     * @param $title
+     * @return mixed
+     * @throws \MrwangTc\QichachaApi\Exceptions\HttpException
+     * @throws \MrwangTc\QichachaApi\Exceptions\InvalidArgumentException
+     */
+    public function SearchWide($title)
+    {
+        $this->getHeader();
+        $params = ['keyword' => $title];
+        $this->getParams($params);
+        $url            = $this->config['domain'] . '/ECIV4/SearchWide';
+        $this->baseData = $this->methodGetDopost($url);
 
+        return $this->getData();
+    }
+
+    /**
+     * Notes   : 企业工商数据查询(企业工商详情)
+     * @Date   : 2021/6/29 11:29
+     * @Author : Mr.wang
+     * @param $title
+     * @return mixed
+     * @throws \MrwangTc\QichachaApi\Exceptions\HttpException
+     * @throws \MrwangTc\QichachaApi\Exceptions\InvalidArgumentException
+     */
+    public function GetBasicDetailsByName($title)
+    {
+        $this->getHeader();
+        $params = ['keyword' => $title];
+        $this->getParams($params);
+        $url            = $this->config['domain'] . '/ECIV4/GetBasicDetailsByName';
+        $this->baseData = $this->methodGetDopost($url);
+
+        return $this->getData();
+    }
 
 }
