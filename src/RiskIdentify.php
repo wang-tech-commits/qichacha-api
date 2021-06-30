cf1b0d828c85f524976b5eb54d4aa50f005a999b
汪晓雨 <15124532157@163.com>
2021-06-30 11:51:27 +0800

    新增几个接口

--- src/RiskIdentify.php
+++ src/RiskIdentify.php
@@ -48,4 +48,76 @@ class RiskIdentify extends BaseApi
         return $this->getData();
     }
 
+    /**
+     * Notes   : 失信核查
+     * @Date   : 2021/6/30 11:33
+     * @Author : Mr.wang
+     * @param $title
+     * @return mixed
+     * @throws \MrwangTc\QichachaApi\Exceptions\HttpException
+     * @throws \MrwangTc\QichachaApi\Exceptions\InvalidArgumentException
+     */
+    public function shiXinCheck($title)
+    {
+        $params         = ['searchKey' => $title];
+        $url            = '/ShixinCheck/GetList';
+        $this->baseData = $this->methodGetHttp($url, $params);
+
+        return $this->getData();
+    }
+
+    /**
+     * Notes   : 经营异常核查
+     * @Date   : 2021/6/30 11:37
+     * @Author : Mr.wang
+     * @param $title
+     * @return mixed
+     * @throws \MrwangTc\QichachaApi\Exceptions\HttpException
+     * @throws \MrwangTc\QichachaApi\Exceptions\InvalidArgumentException
+     */
+    public function exceptionCheck($title)
+    {
+        $params         = ['searchKey' => $title];
+        $url            = '/ExceptionCheck/GetList';
+        $this->baseData = $this->methodGetHttp($url, $params);
+
+        return $this->getData();
+    }
+
+    /**
+     * Notes   : 税收违法核查
+     * @Date   : 2021/6/30 11:42
+     * @Author : Mr.wang
+     * @param $title
+     * @return mixed
+     * @throws \MrwangTc\QichachaApi\Exceptions\HttpException
+     * @throws \MrwangTc\QichachaApi\Exceptions\InvalidArgumentException
+     */
+    public function taxIllegalCheck($title)
+    {
+        $params         = ['searchKey' => $title];
+        $url            = '/TaxIllegalCheck/GetList';
+        $this->baseData = $this->methodGetHttp($url, $params);
+
+        return $this->getData();
+    }
+
+    /**
+     * Notes   : 行政处罚核查
+     * @Date   : 2021/6/30 11:49
+     * @Author : Mr.wang
+     * @param $title
+     * @return mixed
+     * @throws \MrwangTc\QichachaApi\Exceptions\HttpException
+     * @throws \MrwangTc\QichachaApi\Exceptions\InvalidArgumentException
+     */
+    public function adminPenaltyCheck($title)
+    {
+        $params         = ['searchKey' => $title];
+        $url            = '/AdminPenaltyCheck/GetList';
+        $this->baseData = $this->methodGetHttp($url, $params);
+
+        return $this->getData();
+    }
+
 }
